56dfc19b679bcea33739c7b2a79fb6939ac16efa
Abhay H Kashyap <abhayastro1004@gmail.com>
2021-12-02 14:23:22 +0530

    more css updates

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,47 @@
 <template>
-    <div id="nav">
-      <span class="nav-element"><router-link to="/">Home</router-link></span>  
-      <span class="nav-element"><router-link to="/about">About</router-link></span>  
-      <span class="nav-element"><router-link to="/class">Class</router-link></span>  
-      <span class="nav-element"><router-link to="/subject">Temp subject(remove later)</router-link></span>  
-    </div>
+  <div id="nav">
+    <span class="nav-element"><router-link to="/">Home</router-link></span>
+    <span class="nav-element"
+      ><router-link to="/about">About</router-link></span
+    >
+    <span class="nav-element"
+      ><router-link to="/class">Class</router-link></span
+    >
+    <span class="nav-element"
+      ><router-link to="/subject">Temp subject(remove later)</router-link></span
+    >
+  </div>
 </template>
 
-<style> 
-
+<style>
 #nav {
   position: sticky;
   top: 0;
   padding: 3vh 5vw;
-  background: #92B1ED 
+  background: #92b1ed;
 }
 
 #nav a {
-  font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  font-weight: bold;
 }
 
+#nav a:hover {
+  text-decoration: underline;
+}
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #108f56;
 }
 
-.nav-element{
-  padding: 0 1vw
+.nav-element {
+  padding: 0 1vw;
 }
 </style>
 
 <script>
-export default ({
-  name : 'Navbar'
-})
+export default {
+  name: "Navbar",
+};
 </script>
 
