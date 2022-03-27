1f7b77455b01507b9f84b5e2938373d3812b9287
akhil <akhile76@gmail.com>
2022-03-27 16:01:04 +0530

    switched to vue3

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
 
 const routes = [
   {
@@ -39,10 +36,9 @@ const routes = [
     
 ];
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 });
 
 export default router;
