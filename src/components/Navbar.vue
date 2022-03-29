a6372ee952f3d5404c82da19375c54657abdf88f
akhil <akhile76@gmail.com>
2022-03-29 13:46:41 +0530

    added transition for previous chapter

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -156,7 +156,7 @@
             )
           "
     
-      class="max-h-24 m-auto fixed inset-y-24 bottom-4  bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none float-right">
+      class="hover:translate-x-9 -left-8 w-10 max-h-24 m-auto fixed inset-y-24  bottom-4  bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none float-right">
       <svg viewBox="0 0 20 20" enable-background="new 0 0 20 20" class="w-6 h-6 inline-block">
             <path fill="#FFFFFF" d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
                                     C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
