b9f8b70d82ac23331c61e1a7c50af3b65d833309
akhil <akhile76@gmail.com>
2022-03-31 20:52:11 +0530

    added headlessui

--- src/components/NavbarV2.vue
+++ src/components/NavbarV2.vue
@@ -29,7 +29,7 @@
                 :class="[
                   'w-full rounded-lg px-4 py-2 text-xl font-medium leading-5 text-blue-700',
                   'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                  selected
+                  true
                     ? 'bg-white shadow'
                     : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
                 ]"
@@ -38,25 +38,48 @@
               </li>
             </router-link>
           </ul>
-          <ul class="felx md:hidden">
-            <li class="group flex">
-              temp
-              <svg
-                class="fill-current"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"
-                />
-              </svg>
-              <ul class="bg-base-100 hidden p-2 group-hover:block">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-          </ul>
+          <span class="flex md:hidden">
+            <!-- delete this later -->
+            <div class="text-right">
+              <Menu as="div" class="relative inline-block text-left">
+                <div>
+                  <MenuButton
+                    class="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-base font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                  >
+                    Topics
+                  </MenuButton>
+                </div>
+
+                <transition
+                  enter-active-class="transition duration-100 ease-out"
+                  enter-from-class="transform scale-95 opacity-0"
+                  enter-to-class="transform scale-100 opacity-100"
+                  leave-active-class="transition duration-75 ease-in"
+                  leave-from-class="transform scale-100 opacity-100"
+                  leave-to-class="transform scale-95 opacity-0"
+                >
+                  <MenuItems
+                    class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  >
+                    <div class="px-1 py-1">
+                      <MenuItem v-slot="{ active }">
+                        <button
+                          :class="[
+                            active
+                              ? 'bg-violet-500 text-white'
+                              : 'text-gray-900',
+                            'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                          ]"
+                        >
+                          Edit
+                        </button>
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </transition>
+              </Menu>
+            </div>
+          </span>
         </span>
 
         <!-- <button
@@ -72,6 +95,7 @@
 <script>
 import router from "../router";
 import links from "../assets/links.js";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 console.log(links[1]["ENG"][1]);
 console.log(Object.keys(links[1]["ENG"][1]));
 
@@ -80,6 +104,12 @@ export default {
     rout(dest) {
       router.push(dest);
     },
+  },
+  components: {
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem,
   },
   data: function () {
     return {
