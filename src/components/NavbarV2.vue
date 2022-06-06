5bc1689ffa53cdd08b51a17e0b9dfcabbcae63ce
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-06-06 21:15:26 +0530

    Updated the Navbar

--- src/components/NavbarV2.vue
+++ src/components/NavbarV2.vue
@@ -28,7 +28,7 @@
               <li
                 class="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-base font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
               >
-                {{ topics }}
+                {{ dict[topics] === undefined ? topics : dict[topics] }}
               </li>
             </router-link>
           </ul>
@@ -78,8 +78,7 @@
                             ]"
                           >
                             {{
-                              topics.charAt(0).toUpperCase() +
-                              topics.substr(1).toLowerCase()
+                              dict[topics] === undefined ? topics : dict[topics]
                             }}
                           </button>
                         </MenuItem>
@@ -124,6 +123,13 @@ export default {
   data: function () {
     return {
       links: links,
+      dict: {
+        activity: "Activity Sheets",
+        ar: "AR Video",
+        chart: "Charts",
+        video: "Video",
+        ppt: "PPTs",
+      },
     };
   },
 };
